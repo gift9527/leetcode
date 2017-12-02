78ff908ab818120615144d35b7fb461b16396ece
taoming <taoming@192.168.0.100>
2017-12-02 23:30:33 +0800

    insist

---
+++ algorithm/3Sum.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        nums.sort()
+        if len(nums) < 3:
+            return []
+        for i in xrange(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                    continue
+            l, r = i + 1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s > 0:
+                    r = r - 1
+                elif s < 0:
+                    l = l + 1
+                else:
+                    result.append([nums[i],nums[l],nums[r]])
+                    while l < r and nums[l] == nums[l+1]:
+                        l = l + 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r = r -1
+                    l = l + 1
+                    r = r -1
+        return result
+
+
+
+
+if __name__ == "__main__":
+    a = Solution()
+    print a.threeSum(
+        [13, -4, 1, 3, -1, -1, 5, -11, 13, 9, 4, 7, 5, -5, -13, -4, 8, -3, 14, -4, 14, 6, 7, 11, 4, -6, -5, -9, 14, 3,
+         -9, 12, -15, 0, -8, -9, 14, 4, -5, 4, -1, -15, -12, -11, -13, -9, 1, 3, -5, 0, 14, -6, 13, -1, 12, 2, 8, -7, 9,
+         0, 11, 7, -11, 3, -8, -11, 1, 13, 8, 4, -5, 14, 4, -2, 11, -2, -4, -3, -14, 6, 4, 8, 7, 3, -8, 5, 12, 7, 5, -2,
+         -8, -7, 13, -11, 12, 12, -7, -10, 11, -14])
---
+++ algorithm/3SumClosest.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        result = []
+        nums.sort()
+        if len(nums) < 3:
+            return None
+
+        result = nums[1] + nums[0] + nums[2]
+        for i in xrange(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                    continue
+            l, r = i + 1, len(nums)-1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s == target:
+                    return target
+                if abs(s-target) < abs(result-target):
+                    result = s
+                if s < target:
+                    l = l+1
+                else:
+                    r = r - 1
+
+        return result
+
+if __name__ == "__main__":
+    a = Solution()
+    a.threeSumClosest([0,0,0],1)
